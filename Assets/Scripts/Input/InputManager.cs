e1a7251052eceb15874dbd263f63609e49451db2
Swanthie <sy1997@gmail.com>
2021-10-18 21:06:57 +0300

    + Add: Basic input system

---
+++ Assets/Scripts/Input/InputManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Colombus.Inputs
+{
+    public class InputManager : MonoBehaviour
+    {
+        [SerializeField] private ScriptableInputData _inputData;
+
+        private void Update()
+        {
+            _inputData.ProcessInput();
+        }
+    }
+}
---
+++ Assets/Scripts/Input/ScriptableInputData.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Colombus.Inputs
+{
+    [CreateAssetMenu(fileName = "InputData", menuName = "Input/New Input Data")]
+    public class ScriptableInputData : ScriptableObject
+    {
+        [SerializeField] private float _horizontal;
+        public float Horizontal { get => _horizontal; private set => _horizontal = value; }
+
+        //[SerializeField] private float _vertical;
+        //public float Vertical { get => _vertical; private set => _vertical = value; }
+
+        public void ProcessInput()
+        {
+            Horizontal = Input.GetAxis("Horizontal");
+            //Vertical = Input.GetAxis("Vertical");
+        }
+    }
+}
